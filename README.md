f955bf111474ed1aa81aab5fc10f67f8aeb8da32
Shivkumar Jaiswal <sh224797@dal.ca>
2018-06-21 14:55:16 -0300

    cleaning repo

--- README.md
+++
@@ -1,7 +0,0 @@
-# Group16
-Project repo for Group 16
-
-1   Hemant  
-2   Lakshmi Suresh Kumar Gangumalla   lskg@dal.ca  
-3   Pratik Kapoor					  pratik.kapoor@dal.ca  
-4   Shiv Jaiswal					    
--- web.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-  return 'hello group16 -hemant shiv pratik'
-  return 'this is my dev branch-pratik'
-  return 'comment from devenv_hemant'
