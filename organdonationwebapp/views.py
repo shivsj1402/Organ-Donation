9819402626cfa7fe6990ea36d7f58b3761897ddd
Pratik Kapoor <pratik.kapoor@dal.ca>
2018-06-28 16:03:19 -0300

    Changes in views.py for adding routes

--- organdonationwebapp/views.py
+++ organdonationwebapp/views.py
@@ -13,3 +13,11 @@ def takeToHospital():
 @app.route('/helloworld', methods=['GET'])
 def takeToHelloWorld():
     return "<p>hello world test</p>"
+
+@app.route('/login', methods=['GET'])
+def takeToAdmin():
+    return render_template('loginpage.html')
+
+@app.route('/signup', methods=['GET'])
+def registerHospital():
+    return render_template('signup.html')
