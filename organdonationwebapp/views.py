79bac50d54437112bfea47a0f43a8d270bdc0c44
Pratik Kapoor <pratik.kapoor@dal.ca>
2018-07-08 12:10:20 -0300

    Admin login and homepage features

--- organdonationwebapp/views.py
+++ organdonationwebapp/views.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
-from organdonationwebapp import app
+from flask import Flask, render_template, request, redirect, url_for
+from organdonationwebapp import app, sc
+import mysql.connector
 
-
-@app.route('/admin', methods=['GET'])
-def welcomeToAdminPortal():
-    return render_template('adminlogin.html')
+# @app.route('/admin', methods=['GET'])
+# def welcomeToAdminPortal():
+#     return render_template('adminlogin.html')
 
 @app.route('/hospital', methods=['GET'])
 def takeToHospital():
@@ -14,9 +14,9 @@ def takeToHospital():
 def takeToHelloWorld():
     return "<p>hello world test</p>"
 
-@app.route('/login', methods=['GET'])
-def takeToAdmin():
-    return render_template('loginpage.html')
+# @app.route('/login', methods=['GET'])
+# def takeToAdmin():
+#     return render_template('loginpage.html')
 
 @app.route('/signup', methods=['GET'])
 def registerHospital():
@@ -25,3 +25,23 @@ def registerHospital():
 @app.route('/hospitaldonor', methods=['GET'])
 def hospitalDonorPage():
     return render_template('DonorReceiverRequest.html')
+
+@app.route('/adminlogin', methods=['GET','POST'])
+def adminLoginPage():
+    if request.method == 'POST':
+        hospitaldata = request.form
+        email =hospitaldata['emailID']
+        password =hospitaldata['password']
+        user = sc.adminLoginAuthentication(email, password)
+        if(user):
+            return redirect(url_for('adminHomepage', username=user))
+        else:
+            return 'Authentication failed!!'
+    return render_template('adminlogin.html')
+
+@app.route('/adminhome/<username>', methods=['GET','POST'])
+def adminHomepage(username=None):
+    hospitallist = sc.getHospitalList()
+    if(hospitallist):
+        return render_template('adminhome.html', list=hospitallist,username=username)
+    return render_template('adminhome.html', username=username)
