30ff6e7518c6239a2bd465d21dae7476306d2f80
Shivkumar Jaiswal <shivsj1402@gmail.com>
2018-07-18 15:22:54 -0300

    changes

--- organdonationwebapp/User/Models.py
+++ organdonationwebapp/User/Models.py
@@ -6,11 +6,11 @@ class UserModel(SqlClient):
 
 
     def userRegistration(self, first_name, last_name, phone_number, email, sex, dob, address, province, city, hospital, bloodgroup, usertype, organ):
-        for item in organ:
-            try:
+        try:
+            for item in organ:
                 self.cursor.callproc('userregistration',[first_name, last_name, phone_number, email, sex, dob, address, province, city, hospital, bloodgroup, usertype, item])
                 self.connection.commit()
-                return True
-            except Exception as err:
-                print(err)
-                return False
+            return True
+        except Exception as err:
+            print(err)
+            return False
