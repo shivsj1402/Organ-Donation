678028c08e534f6467e6903bfd510ef4f49b37c0
Shivkumar Jaiswal <shivsj1402@gmail.com>
2018-07-19 14:44:10 -0300

    updates

--- organdonationwebapp/Hospital/Hospital.py
+++ organdonationwebapp/Hospital/Hospital.py
@@ -1,5 +1,5 @@
 from organdonationwebapp import hc
-import bcrypt
+# import bcrypt
 
 class Hospital(object):
     def initialize(self,hospitalJson,certificateFile=None):
@@ -14,8 +14,8 @@ class Hospital(object):
 
 
     def registerHospital(self):
-        self.encrypted_password = bcrypt.hashpw(self.password.encode('utf-8'), bcrypt.gensalt())
-        if(hc.hospitalRegistration(self.hospitalName, self.emailID, self.phone, self.address, self.province, self.city, self.encrypted_password, self.data)):
+        # self.encrypted_password = bcrypt.hashpw(self.password.encode('utf-8'), bcrypt.gensalt())
+        if(hc.hospitalRegistration(self.hospitalName, self.emailID, self.phone, self.address, self.province, self.city, self.password, self.data)):
             return True
         else:
             return False
@@ -25,7 +25,7 @@ class Hospital(object):
         result = hc.hospitalLoginAuthentication(self.emailID)
         if(result):
             try:
-                if(bcrypt.checkpw(self.password.encode('utf-8'), result[0][6].encode('utf-8'))):
+                if(self.password == result[0][6]):
                     return result
             except Exception as err:
                 print(err)
