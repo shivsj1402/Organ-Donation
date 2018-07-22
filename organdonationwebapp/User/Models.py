a892e303611ee9ede3588a93dad32d1ae4ec38d3
Shivkumar Jaiswal <shivsj1402@gmail.com>
2018-07-21 22:25:51 -0300

    Request date Added

--- organdonationwebapp/User/Models.py
+++ organdonationwebapp/User/Models.py
@@ -1,4 +1,5 @@
 from organdonationwebapp.models.SqlClient import SqlClient
+import datetime
 
 class UserModel(SqlClient):
     def __init__(self):
@@ -6,9 +7,10 @@ class UserModel(SqlClient):
 
 
     def userRegistration(self, user):
+        requestdate =datetime.datetime.today().strftime('%Y-%m-%d')
         try:
             for item in user.organ:
-                self.cursor.callproc('userregistration',[user.first_name, user.last_name, user.phone_number, user.email, user.sex, user.dob, user.address, user.province, user.city, user.hospital, user.bloodgroup, user.usertype,item])
+                self.cursor.callproc('userregistration',[user.first_name, user.last_name, user.phone_number, user.email, user.sex, user.dob, user.address, user.province, user.city, user.hospital, user.bloodgroup, user.usertype,requestdate,item])
                 self.connection.commit()
             return True
         except Exception as err:
